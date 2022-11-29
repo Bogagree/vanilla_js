04e0c95f61e6beb27297e88e638dccf769b9783d
Bogagree <bogatireshka@gmail.com>
2022-11-29 22:31:26 +0300

    feat: sortedNewArray => with for cycle

--- src/lessons/lesson0_sobes/sobes_tasks.js
+++ src/lessons/lesson0_sobes/sobes_tasks.js
@@ -1,17 +1,24 @@
 // дан массив arr = [1,1,2,2,2,5,5,5,2,7] нужно вернуть уникальный массив, отсортированный по частоте встречаемости. 2 встречается чаще всего, потом 5, и т.д [2,5,1,7]
 
-const arr = [1,1,2,2,2,5,5,5,2,7]
+const arr = [1, 1, 2, 2, 2, 5, 5, 5, 2, 7]
 
 const sortedNewArr = (arr) => {
-
-    const obj = arr.reduce((acc,el) => {
-        // const currCount = acc[el] || 0;
-        // return {...acc, [el]: currCount + 1 }
-        acc[el] = (acc[el] || 0 ) + 1
-        return acc
-    }, {} )
+    // const obj = arr.reduce((acc,el) => {
+    //     // const currCount = acc[el] || 0;
+    //     // return {...acc, [el]: currCount + 1 }
+    //     acc[el] = (acc[el] || 0 ) + 1
+    //     return acc
+    // }, {} )
+    const obj = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]]) {
+            obj[arr[i]]++
+        } else {
+            obj[arr[i]] = 1
+        }
+    }
     console.log(obj)
-    const newArr = Object.keys(obj).sort( (a,b)=> obj[b] - obj[a])
+    const newArr = Object.keys(obj).sort((a, b) => obj[b] - obj[a])
     console.log(newArr)
     return newArr
 }
